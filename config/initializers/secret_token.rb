3795a9414475be79dc28a04534159dd905b5e93c
Aja Hammerly <aja.hammerly+github@gmail.com>
2013-06-04 19:38:32 -0700

    Basic rails scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StuporSlow::Application.config.secret_token = '3b8d1bdfb04c990bdd84761198ae0005108de4bdc1c75444b96870208679a1d72335e9b67cf90917b1419fc22e35d29fb1247c994f6bd2a489c2f04925441bfc'
