6e25b88446ab7896b09805e30c9fc4dd5bf72a2a
Aja Hammerly <aja.hammerly+github@gmail.com>
2013-08-23 17:26:41 -0500

    Move style from workouts_helper to visualize_helper

--- app/helpers/visualize_helper.rb
+++ app/helpers/visualize_helper.rb
@@ -1,2 +1,13 @@
 module VisualizeHelper
+  def style_avg(rel_time, target_time)
+    pct = rel_time / target_time.to_f
+    case
+    when pct > 1.1 then
+      "color: green"
+    when pct < 0.9 then
+      "color: red; font-weight: bold"
+    else
+      ""
+    end
+  end
 end
