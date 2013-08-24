e81d1781f8a1f730f87c7c0114070f828265b80d
Aja Hammerly <aja.hammerly+github@gmail.com>
2013-08-24 12:06:09 -0500

    Add colored formatting to averages

--- app/helpers/visualize_helper.rb
+++ app/helpers/visualize_helper.rb
@@ -1,13 +1,13 @@
 module VisualizeHelper
   def style_avg(rel_time, target_time)
-    pct = rel_time / target_time.to_f
+    pct = (rel_time / target_time.to_f).round(2)
     case
     when pct > 1.1 then
-      "color: green"
+      "<span style='color: green'>#{pct}</span>".html_safe
     when pct < 0.9 then
-      "color: red; font-weight: bold"
+      "<span style='color: red; font-weight: bold'>#{pct}</span>".html_safe
     else
-      ""
+      "<span>#{pct}</span>".html_safe
     end
   end
 end
