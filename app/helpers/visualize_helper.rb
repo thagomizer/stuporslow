dd285c0e7ba95069a689908b1c694e62cc884a6e
Aja Hammerly <aja.hammerly+github@gmail.com>
2013-08-24 12:15:20 -0500

    Show time not decimal compared to goal

--- app/helpers/visualize_helper.rb
+++ app/helpers/visualize_helper.rb
@@ -3,11 +3,11 @@ module VisualizeHelper
     pct = (rel_time / target_time.to_f).round(2)
     case
     when pct > 1.1 then
-      "<span style='color: green'>#{pct}</span>".html_safe
+      "<span style='color: green'>#{rel_time}</span>".html_safe
     when pct < 0.9 then
-      "<span style='color: red; font-weight: bold'>#{pct}</span>".html_safe
+      "<span style='color: red; font-weight: bold'>#{rel_time}</span>".html_safe
     else
-      "<span>#{pct}</span>".html_safe
+      "<span>#{rel_time}</span>".html_safe
     end
   end
 end
