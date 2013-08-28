b876ac0a3588f6d9303ced85bea7e6bc8674a59b
Aja Hammerly <aja.hammerly+github@gmail.com>
2013-08-27 19:54:44 -0700

    Make visualize_controller_test pass again

--- test/controllers/visualize_controller_test.rb
+++ test/controllers/visualize_controller_test.rb
@@ -1,6 +1,11 @@
-require "test_helper"
+require File.expand_path("../../test_helper", __FILE__)
 
 class VisualizeControllerTest < ActionController::TestCase
+  def setup
+    fred = create_athlete
+    sign_in fred
+  end
+
   def test_get_graph
     get :graph
     assert_response :success
