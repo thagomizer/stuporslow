39a689d7f60ec9467afbfb20492b648658a56a4d
Aja Hammerly <aja.hammerly+github@gmail.com>
2013-12-17 19:30:49 -0800

    Add tests for relations and attr_accessibles

--- test/models/athlete_test.rb
+++ test/models/athlete_test.rb
@@ -7,10 +7,34 @@ class AthleteTest < ActiveSupport::TestCase
                           :email      => "fred@example.com",
                           :password   => "password")
     assert athlete.save
+    athlete.reload
+
+    assert_equal "Fred", athlete.first_name
+    assert_equal "Rogers", athlete.last_name
+    assert_equal "fred@example.com", athlete.email
+    assert_equal "password", athlete.password
+
+    assert_equal [], athlete.workouts
+    assert_equal [], athlete.workout_templates
+  end
+
+  def test_athletes_have_workouts
+    athlete = create_athlete
+    athlete.workouts << Workout.new
+    athlete.workouts << Workout.new
+
+    assert_equal 2, athlete.workouts.count
+  end
+
+  def test_athletes_have_workout_templates
+    athlete = create_athlete
+    athlete.workout_templates << WorkoutTemplate.new
+    athlete.workout_templates << WorkoutTemplate.new
+
+    assert_equal 2, athlete.workout_templates.count
   end
 
   def test_lifts_for_exercise
-
     leg_ex = create_exercise("Leg Extension")
     lifts = []
 
