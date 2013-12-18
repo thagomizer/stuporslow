13cb8c9b3fefea206d2bd57c3b49d3c8982d7e7f
Aja Hammerly <aja.hammerly+github@gmail.com>
2013-12-17 19:35:38 -0800

    Verify values set during creation to verify attr_accessible

--- test/models/exercise_test.rb
+++ test/models/exercise_test.rb
@@ -4,5 +4,8 @@ class ExerciseTest < ActiveSupport::TestCase
   def test_exercise_creation
     exercise = Exercise.new(:name => "Compound Row")
     assert exercise.save
+    exercise.reload
+
+    assert_equal "Compound Row", exercise.name
   end
 end
